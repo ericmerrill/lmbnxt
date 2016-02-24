02e2244d3b42782d722b2e8d26960e125b61190d
Eric Merrill <merrill@oakland.edu>
2016-02-24 01:32:03 -0500

    DML catch

--- classes/local/data/base.php
+++ classes/local/data/base.php
@@ -174,51 +174,54 @@ abstract class base {
         global $DB;
 
         $existing = $this->get_record();
+        try {
+            if ($existing) {
+                // If this is existing, set the id.
+                $this->__set('id', $existing->id);
+            } else {
+                // Insert the record because it doesn't exist
+                $new = $this->convert_to_db_object();
+                $id = $DB->insert_record(static::TABLE, $new);
+                if (!$id) {
+                    throw new \enrol_lmb\local\exception\message_exception('exception_insert_failure');
+                }
+                $this->__set('id', $id);
+                logging::instance()->log_line('Inserting into database');
+                return;
+            }
 
-        if ($existing) {
-            // If this is existing, set the id.
-            $this->__set('id', $existing->id);
-        } else {
-            // Insert the record because it doesn't exist
+            // Get the current as a db object.
             $new = $this->convert_to_db_object();
-            $id = $DB->insert_record(static::TABLE, $new);
-            if (!$id) {
-                throw new \enrol_lmb\local\exception\message_exception('exception_insert_failure');
-            }
-            $this->__set('id', $id);
-            logging::instance()->log_line('Inserting into database');
-            return;
-        }
 
-        // Get the current as a db object.
-        $new = $this->convert_to_db_object();
+            $needsupdate = false;
+            foreach ($this->dbkeys as $key) {
+                if ($key === 'timemodified') {
+                    // We ignore the timemodified column.
+                    continue;
+                }
 
-        $needsupdate = false;
-        foreach ($this->dbkeys as $key) {
-            if ($key === 'timemodified') {
-                // We ignore the timemodified column.
-                continue;
+                if ($new->$key != $existing->$key) {
+                    // If the values don't match, skip the rest.
+                    $needsupdate = true;
+                    break;
+                }
             }
 
-            if ($new->$key != $existing->$key) {
-                // If the values don't match, skip the rest.
-                $needsupdate = true;
-                break;
+            if (!$needsupdate) {
+                // Nothing to update.
+                logging::instance()->log_line('No database update needed');
+                return;
             }
-        }
 
-        if (!$needsupdate) {
-            // Nothing to update.
-            logging::instance()->log_line('No database update needed');
-            return;
-        }
-
-        if ($DB->update_record('enrol_lmb_person', $new)) {
-            // Updated the record.
-            logging::instance()->log_line('Updated database record');
-        } else {
-            // There was some error.
-            throw new \enrol_lmb\local\exception\message_exception('exception_update_failure');
+            if ($DB->update_record('enrol_lmb_person', $new)) {
+                // Updated the record.
+                logging::instance()->log_line('Updated database record');
+            } else {
+                // There was some error.
+                throw new \enrol_lmb\local\exception\message_exception('exception_update_failure');
+            }
+        } catch (\dml_exception $ex) {
+            throw new \enrol_lmb\local\exception\message_exception('exception_update_failure', '', null, $ex->getMessage());
         }
     }
 
