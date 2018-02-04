e40352f5da4eb495fd281440a460c9adc43741d9
Eric Merrill <merrill@oakland.edu>
2018-02-04 14:39:06 -0500

    Restore some tests

--- tests/data/data_member_person_test.php
+++ tests/data/data_member_person_test.php
@@ -61,100 +61,100 @@ class data_member_person_testcase extends xml_helper {
         $member = $xmlconverter->process_xml_to_data($xmlnode);
         $member = reset($member);
         $member->save_to_db();
-//
-//         // Now make sure it gets updated when we process from LIS (with a message reference).
-//         // Going to set the lis node as inactive to help to verify what is happening.
-//         $lisnode->MEMBERSHIPRECORD->MEMBERSHIP->MEMBER->ROLE->STATUS->set_data('Inactive');
-//         $member = $lisconverter->process_xml_to_data($lisnode);
-//         $member->load_existing();
-//         $member->save_to_db();
-//
-//         $record = $DB->get_record(data\member_person::TABLE, ['id' => $recordid]);
-//         $this->assertInstanceOf(\stdClass::class, $record);
-//         $this->assertEquals('CM-editingteacher-CS10001.201740-1000001', $record->messagereference);
-//         $this->assertEquals('0', $record->status);
-//
-//         // Now, make sure it doesn't go back to empty.
-//         $member = $xmlconverter->process_xml_to_data($xmlnode);
-//         $member = reset($member);
-//         $member->load_existing();
-//         $member->save_to_db();
-//
-//         $record = $DB->get_record(data\member_person::TABLE, ['id' => $recordid]);
-//         $this->assertInstanceOf(\stdClass::class, $record);
-//         $this->assertEquals('CM-editingteacher-CS10001.201740-1000001', $record->messagereference);
-//         $this->assertEquals('1', $record->status);
+
+        // Now make sure it gets updated when we process from LIS (with a message reference).
+        // Going to set the lis node as inactive to help to verify what is happening.
+        $lisnode->MEMBERSHIPRECORD->MEMBERSHIP->MEMBER->ROLE->STATUS->set_data('Inactive');
+        $member = $lisconverter->process_xml_to_data($lisnode);
+        $member->load_existing();
+        $member->save_to_db();
+
+        $record = $DB->get_record(data\member_person::TABLE, ['id' => $recordid]);
+        $this->assertInstanceOf(\stdClass::class, $record);
+        $this->assertEquals('CM-editingteacher-CS10001.201740-1000001', $record->messagereference);
+        $this->assertEquals('0', $record->status);
+
+        // Now, make sure it doesn't go back to empty.
+        $member = $xmlconverter->process_xml_to_data($xmlnode);
+        $member = reset($member);
+        $member->load_existing();
+        $member->save_to_db();
+
+        $record = $DB->get_record(data\member_person::TABLE, ['id' => $recordid]);
+        $this->assertInstanceOf(\stdClass::class, $record);
+        $this->assertEquals('CM-editingteacher-CS10001.201740-1000001', $record->messagereference);
+        $this->assertEquals('1', $record->status);
     }
 
-//     public function test_log_id() {
-//         global $CFG;
-//
-//         $node = $this->get_node_for_file($CFG->dirroot.'/enrol/lmb/tests/fixtures/lmb/member_person.xml');
-//         $converter = new xml\membership();
-//         $members = $converter->process_xml_to_data($node);
-//
-//         $log = new logging_helper();
-//         $log->set_logging_level(logging::ERROR_NONE);
-//
-//         foreach ($members as $member) {
-//             $member->log_id();
-//
-//             $regex = "|{$member->membersdid}.*{$member->membersdidsource}.*{$member->groupsdid}.*{$member->groupsdidsource}|";
-//             $this->assertRegExp($regex, $log->test_get_flush_buffer());
-//
-//             unset($member->membersdid);
-//
-//             try {
-//                 $member->log_id();
-//                 $this->fail('message_exception expected');
-//             } catch (exception\message_exception $ex) {
-//                 $this->assertInstanceOf(exception\message_exception::class, $ex);
-//                 $expected = get_string('exception_bad_member_person', 'enrol_lmb');
-//                 $this->assertRegExp("|{$expected}|", $ex->getMessage());
-//             }
-//         }
-//     }
-//
-//     public function test_db_save() {
-//         global $CFG, $DB;
-//
-//         $this->resetAfterTest(true);
-//
-//         $node = $this->get_node_for_file($CFG->dirroot.'/enrol/lmb/tests/fixtures/lmb/member_person.xml');
-//         $converter = new xml\membership();
-//         $members = $converter->process_xml_to_data($node);
-//
-//         $log = new logging_helper();
-//         $log->set_logging_level(logging::ERROR_NONE);
-//
-//         foreach ($members as $member) {
-//             // First insert.
-//             $member->save_to_db();
-//             $this->assertRegExp("|Inserting into database|", $log->test_get_flush_buffer());
-//
-//             // Try to save the same object again.
-//             $member->save_to_db();
-//             $this->assertRegExp("|No database update needed|", $log->test_get_flush_buffer());
-//
-//             // Modify the course and try and insert again.
-//             $member->status = (int)!$member->status;
-//             $member->save_to_db();
-//             $this->assertRegExp("|Updated database record|", $log->test_get_flush_buffer());
-//
-//             // Now lets get it from the DB and check it.
-//             $params = array('membersdid' => $member->membersdid, 'membersdidsource' => $member->membersdidsource,
-//                     'groupsdid' => $member->groupsdid, 'groupsdidsource' => $member->groupsdidsource);
-//             $dbrecord = $DB->get_record(data\member_person::TABLE, $params);
-//
-//             $this->assertNotEmpty($dbrecord);
-//
-//             foreach ($dbrecord as $key => $value) {
-//                 if ($key == 'timemodified') {
-//                     // Skip special case.
-//                     continue;
-//                 }
-//                 $this->assertEquals($member->$key, $value, "Key {$key} did not match");
-//             }
-//         }
-//     }
+    public function test_log_id() {
+        global $CFG;
+
+        $node = $this->get_node_for_file($CFG->dirroot.'/enrol/lmb/tests/fixtures/lmb/member_person.xml');
+        $converter = new xml\membership();
+        $members = $converter->process_xml_to_data($node);
+
+        $log = new logging_helper();
+        $log->set_logging_level(logging::ERROR_NONE);
+
+        foreach ($members as $member) {
+            $member->log_id();
+
+            $regex = "|{$member->membersdid}.*{$member->membersdidsource}.*{$member->groupsdid}.*{$member->groupsdidsource}|";
+            $this->assertRegExp($regex, $log->test_get_flush_buffer());
+
+            unset($member->membersdid);
+
+            try {
+                $member->log_id();
+                $this->fail('message_exception expected');
+            } catch (exception\message_exception $ex) {
+                $this->assertInstanceOf(exception\message_exception::class, $ex);
+                $expected = get_string('exception_bad_member_person', 'enrol_lmb');
+                $this->assertRegExp("|{$expected}|", $ex->getMessage());
+            }
+        }
+    }
+
+    public function test_db_save() {
+        global $CFG, $DB;
+
+        $this->resetAfterTest(true);
+
+        $node = $this->get_node_for_file($CFG->dirroot.'/enrol/lmb/tests/fixtures/lmb/member_person.xml');
+        $converter = new xml\membership();
+        $members = $converter->process_xml_to_data($node);
+
+        $log = new logging_helper();
+        $log->set_logging_level(logging::ERROR_NONE);
+
+        foreach ($members as $member) {
+            // First insert.
+            $member->save_to_db();
+            $this->assertRegExp("|Inserting into database|", $log->test_get_flush_buffer());
+
+            // Try to save the same object again.
+            $member->save_to_db();
+            $this->assertRegExp("|No database update needed|", $log->test_get_flush_buffer());
+
+            // Modify the course and try and insert again.
+            $member->status = (int)!$member->status;
+            $member->save_to_db();
+            $this->assertRegExp("|Updated database record|", $log->test_get_flush_buffer());
+
+            // Now lets get it from the DB and check it.
+            $params = array('membersdid' => $member->membersdid, 'membersdidsource' => $member->membersdidsource,
+                    'groupsdid' => $member->groupsdid, 'groupsdidsource' => $member->groupsdidsource);
+            $dbrecord = $DB->get_record(data\member_person::TABLE, $params);
+
+            $this->assertNotEmpty($dbrecord);
+
+            foreach ($dbrecord as $key => $value) {
+                if ($key == 'timemodified') {
+                    // Skip special case.
+                    continue;
+                }
+                $this->assertEquals($member->$key, $value, "Key {$key} did not match");
+            }
+        }
+    }
 }
