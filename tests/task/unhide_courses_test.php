a57b07fbbbf2c4f6d7e5dfb1d8ac27ff68f43d58
Eric Merrill <merrill@oakland.edu>
2018-02-27 09:55:26 -0500

    Test updates

--- tests/task/unhide_courses_test.php
+++ tests/task/unhide_courses_test.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Tests for the upgrade lib.
+ * Tests for the unhide courses test.
  *
  * @package    enrol_lmb
  * @author     Eric Merrill <merrill@oakland.edu>
@@ -26,83 +26,33 @@
 defined('MOODLE_INTERNAL') || die();
 
 use enrol_lmb\local\data;
+use enrol_lmb\local\moodle;
+use enrol_lmb\task\unhide_courses;
 
 global $CFG;
-require_once($CFG->dirroot.'/enrol/lmb/upgradelib.php');
+
 require_once($CFG->dirroot.'/enrol/lmb/tests/helper.php');
 
-class upgradelib_test extends xml_helper {
+class unhide_courses_test extends xml_helper {
 
-    public function test_enrol_lmb_upgrade_promote_column() {
-        global $DB;
+    public function test_course_unhide() {
+        global $CFG, $DB;
 
         $this->resetAfterTest(true);
+        $task = new unhide_courses();
 
-        $record1 = new stdClass();
-        $record1->timemodified = time();
-        $record1->givenname = 'First1';
-        $record1->sdid = '1';
-        $record1->id = $DB->insert_record(data\person::TABLE, $record1);
+        $node = $this->get_node_for_file($CFG->dirroot.'/enrol/lmb/tests/fixtures/lmb/data/section.xml');
+        $converter = new xml\group_section();
+        $section = $converter->process_xml_to_data($node);
 
-        $record2 = new stdClass();
-        $record2->timemodified = time();
-        $record2->sdid = '2';
-        $record2->givenname = 'First2';
-        $additional = new stdClass();
-        $additional->primaryrole = 'Staff';
-        $record2->additional = json_encode($additional, JSON_UNESCAPED_UNICODE);
-        $record2->id = $DB->insert_record(data\person::TABLE, $record2);
+        $moodlecourse = new moodle\course();
+        $moodlecourse->convert_to_moodle($section);
 
-        $record3 = new stdClass();
-        $record3->timemodified = time();
-        $record3->sdid = '3';
-        $record3->givenname = 'First3';
-        $additional = new stdClass();
-        $additional->primaryrole = 'Bob!!';
-        $additional->otherkey = 'Something';
-        $record3->additional = json_encode($additional, JSON_UNESCAPED_UNICODE);
-        $record3->id = $DB->insert_record(data\person::TABLE, $record3);
+        $curtime = time();
+        $starttoday = mktime(0, 0, 0, date('n', $curtime), date('j', $curtime), date('Y', $curtime));
+//DAYSECS
 
-        // This should cause a dml_exception, which should be cleanly caught.
-        $long = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefgh";
-        $record4 = new stdClass();
-        $record4->timemodified = time();
-        $record4->sdid = '4';
-        $record4->givenname = 'First4';
-        $additional = new stdClass();
-        $additional->primaryrole = $long;
-        $record4->additional = json_encode($additional, JSON_UNESCAPED_UNICODE);
-        $record4->id = $DB->insert_record(data\person::TABLE, $record4);
-
-        enrol_lmb_upgrade_promote_column(data\person::TABLE, 'primaryrole');
-
-        $record = $DB->get_record(data\person::TABLE, ['id' => $record1->id]);
-        $this->assertEquals('First1', $record->givenname);
-        $this->assertEquals('1', $record->sdid);
-        $this->assertNull($record->primaryrole);
-
-        $record = $DB->get_record(data\person::TABLE, ['id' => $record2->id]);
-        $this->assertEquals('First2', $record->givenname);
-        $this->assertEquals('2', $record->sdid);
-        $this->assertEquals('Staff', $record->primaryrole);
-        $additional = json_decode($record->additional);
-        $this->assertCount(0, (array)$additional);
-
-        $record = $DB->get_record(data\person::TABLE, ['id' => $record3->id]);
-        $this->assertEquals('First3', $record->givenname);
-        $this->assertEquals('3', $record->sdid);
-        $this->assertEquals('Bob!!', $record->primaryrole);
-        $additional = json_decode($record->additional);
-        $this->assertCount(1, (array)$additional);
-        $this->assertEquals('Something', $additional->otherkey);
-
-        $record = $DB->get_record(data\person::TABLE, ['id' => $record4->id]);
-        $this->assertEquals('First4', $record->givenname);
-        $this->assertEquals('4', $record->sdid);
-        $this->assertNull($record->primaryrole);
-        $additional = json_decode($record->additional);
-        $this->assertCount(1, (array)$additional);
-        $this->assertEquals($long, $additional->primaryrole);
+        $task->execute();
     }
 
 }
